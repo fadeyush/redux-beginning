dd626fd772f1d65935f9ed2746d6328676af9e85
fadeyush <a.fadeyush@gmail.com>
2024-02-08 19:39:42 +0300

    комментарии

--- src/asyncActions/customers.jsx
+++ src/asyncActions/customers.jsx
@@ -7,3 +7,8 @@ export const fetchCustomers = () => {
             .then(json=>dispatch(addManyCustomerAction(json)))
     }
 }
+
+//redux-thunk - это middleware, который позволяет внутри сторонних функций использовать dispatch
+//Он внутри себя опрокилывает в эту функцию параметром dispatch и можно им пользоваться
+
+//middleware - позволяет обернуть метод стора dispatch
